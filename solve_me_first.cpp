54ad5aef36b86780b296befff6b937bf3eaf30d1
João Batista de Aguiar Júnior <junior_jack007@hotmail.com>
2025-04-07 07:31:45 -0300

    refactor: renomeia função e adiciona comentários explicativos conforme sugestão da revisão

--- solve_me_first.cpp
+++ solve_me_first.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int solveMeFirst(int a, int b) {
+// Função que retorna a soma de dois números inteiros
+int sumOfTwoNumbers(int a, int b) {
     int soma = a + b;
     return soma;
 }
 
 int main() {
     int num1, num2;
+
+    // Entrada dos dois números
     cin >> num1 >> num2;
-    int sum = solveMeFirst(num1, num2);
+    // Chamada da função para somar os números
+    int sum = sumOfTwoNumbers(num1, num2);
+
+    // Impressão do resultado
     cout << sum << endl;
     return 0;
 }
